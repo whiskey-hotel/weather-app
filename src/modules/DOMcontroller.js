63f52976aba058c64fab34f69344a2a86a520dee
Justin Thornton <jathom15@gmail.com>
2022-05-02 00:39:17 -0500

    Serve remainder of data to page

--- src/modules/DOMcontroller.js
+++ src/modules/DOMcontroller.js
@@ -19,6 +19,44 @@ class DOMObjects {
     document.getElementById('currentWeatherDescription').textContent =
       DOMObjects.userData.description;
 
+    document.getElementById('currentWeatherTemp').textContent = DOMObjects.userData.temp;
+
+    document.getElementById('currentWeatherHi').textContent = DOMObjects.userData.hi;
+
+    document.getElementById('currentWeatherLow').textContent = DOMObjects.userData.low;
+
+    const days = [
+      DOMObjects.userData.day1,
+      DOMObjects.userData.day2,
+      DOMObjects.userData.day3,
+      DOMObjects.userData.day4,
+      DOMObjects.userData.day5,
+      DOMObjects.userData.day6,
+      DOMObjects.userData.day7,
+    ];
+
+    for (let i = 0; i < days.length; i += 1) {
+      document.getElementById(
+        `day${i + 1}Icon`,
+      ).src = `http://openweathermap.org/img/wn/${days[i].weather[0].icon}@2x.png`;
+
+      document.getElementById(`day${i + 1}OfTheWeek`).textContent = days[i].dt;
+      document.getElementById(`day${i + 1}Hi`).textContent = days[i].temp.max;
+      document.getElementById(`day${i + 1}Low`).textContent = days[i].temp.min;
+    }
+
+    document.getElementById('detailTempsValue').textContent = DOMObjects.userData.temp;
+    document.getElementById('detailFeelsLikeValue').textContent = DOMObjects.userData.feelsLike;
+    document.getElementById('detailHumidityValue').textContent = DOMObjects.userData.humidity;
+    document.getElementById('detailDewPointValue').textContent = DOMObjects.userData.dewpoint;
+    document.getElementById('detailPressureValue').textContent = DOMObjects.userData.pressure;
+    document.getElementById('detailUVIndexValue').textContent = DOMObjects.userData.UVIndex;
+    document.getElementById('detailWindSpeedValue').textContent = DOMObjects.userData.windSpeed;
+    document.getElementById('detailWindDirectionValue').textContent =
+      DOMObjects.userData.windDirection;
+    document.getElementById('detailSunriseValue').textContent = DOMObjects.userData.sunrise;
+    document.getElementById('detailSunsetValue').textContent = DOMObjects.userData.sunset;
+
     console.log('success');
     console.log(DOMObjects.userData);
   }
@@ -178,11 +216,17 @@ class DOMObjects {
       elementID: 'dailyForecastContainer',
     });
 
-    for (let i = 0; i < 7; i += 1) {
+    for (let i = 1; i < 8; i += 1) {
       const dayContainer = elementBuilder.newElement({
         element: 'div',
         elementID: `day${i}Container`,
         className: 'dayContainer',
+      });
+
+      const daysOfTheWeek = elementBuilder.newElement({
+        element: 'span',
+        elementID: `day${i}OfTheWeek`,
+        className: 'daysOfTheWeek',
       });
 
       const dayIconContainer = elementBuilder.newElement({
@@ -232,6 +276,7 @@ class DOMObjects {
       });
 
       dailyForecastContainer.appendChild(dayContainer);
+      dayContainer.appendChild(daysOfTheWeek);
       dayContainer.appendChild(dayIconContainer);
       dayIconContainer.appendChild(dayIcon);
       dayContainer.appendChild(dayHiLoContainer);
@@ -256,60 +301,181 @@ class DOMObjects {
       text: 'Weather Details',
     });
 
+    const detailTempsContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailTempsContainer',
+      className: 'detailsContainer',
+    });
+
     const detailTemps = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailTemps',
       className: 'details',
       text: 'Temperature: ',
     });
+
+    const detailTempsValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailTempsValue',
+      className: 'details',
+    });
+
+    const detailFeelsLikeContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailFeelsLikeContainer',
+      className: 'detailsContainer',
+    });
+
     const detailFeelsLike = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailFeelsLike',
       className: 'details',
       text: 'Feels Like: ',
     });
+
+    const detailFeelsLikeValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailFeelsLikeValue',
+      className: 'details',
+    });
+
+    const detailHumidityContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailHumidityContainer',
+      className: 'detailsContainer',
+    });
+
     const detailHumidity = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailHumidity',
       className: 'details',
       text: 'Humidity: ',
     });
+
+    const detailHumidityValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailHumidityValue',
+      className: 'details',
+    });
+
+    const detailDewPointContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailDewPointContainer',
+      className: 'detailsContainer',
+    });
+
     const detailDewPoint = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailDewPoint',
       className: 'details',
       text: 'Dew Point: ',
     });
+    const detailDewPointValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailDewPointValue',
+      className: 'details',
+    });
+
+    const detailPressureContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailPressureContainer',
+      className: 'detailsContainer',
+    });
+
     const detailPressure = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailPressure',
       className: 'details',
       text: 'Pressure: ',
     });
+    const detailPressureValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailPressureValue',
+      className: 'details',
+    });
+
+    const detailUVIndexContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailUVIndexContainer',
+      className: 'detailsContainer',
+    });
+
     const detailUVIndex = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailUVIndex',
       className: 'details',
       text: 'UV Index: ',
     });
+
+    const detailUVIndexValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailUVIndexValue',
+      className: 'details',
+    });
+
+    const detailWindSpeedContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailWindSpeedContainer',
+      className: 'detailsContainer',
+    });
+
     const detailWindSpeed = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailWindSpeed',
       className: 'details',
       text: 'Wind Speed: ',
     });
+
+    const detailWindSpeedValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailWindSpeedValue',
+      className: 'details',
+    });
+
+    const detailWindDirectionContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailWindDirectionContainer',
+      className: 'detailsContainer',
+    });
+
     const detailWindDirection = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailWindDirection',
       className: 'details',
       text: 'Wind Direction: ',
     });
+
+    const detailWindDirectionValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailWindDirectionValue',
+      className: 'details',
+    });
+
+    const detailSunriseContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailSunriseContainer',
+      className: 'detailsContainer',
+    });
+
     const detailSunrise = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailSunrise',
       className: 'details',
       text: 'Sunrise: ',
     });
+
+    const detailSunriseValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailSunriseValue',
+      className: 'details',
+    });
+
+    const detailSunsetContainer = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailSunsetContainer',
+      className: 'detailsContainer',
+    });
+
     const detailSunset = elementBuilder.newElement({
       element: 'div',
       elementID: 'detailSunset',
@@ -317,18 +483,43 @@ class DOMObjects {
       text: 'Sunset: ',
     });
 
+    const detailSunsetValue = elementBuilder.newElement({
+      element: 'div',
+      elementID: 'detailSunsetValue',
+      className: 'details',
+    });
+
     weatherDetailsContainer.appendChild(title);
-    weatherDetailsContainer.appendChild(detailTemps);
-    weatherDetailsContainer.appendChild(detailFeelsLike);
-    weatherDetailsContainer.appendChild(detailHumidity);
-    weatherDetailsContainer.appendChild(detailDewPoint);
-    weatherDetailsContainer.appendChild(detailPressure);
-    weatherDetailsContainer.appendChild(detailUVIndex);
-    weatherDetailsContainer.appendChild(detailWindSpeed);
-    weatherDetailsContainer.appendChild(detailWindSpeed);
-    weatherDetailsContainer.appendChild(detailWindDirection);
-    weatherDetailsContainer.appendChild(detailSunrise);
-    weatherDetailsContainer.appendChild(detailSunset);
+    weatherDetailsContainer.appendChild(detailTempsContainer);
+    detailTempsContainer.appendChild(detailTemps);
+    detailTempsContainer.appendChild(detailTempsValue);
+    weatherDetailsContainer.appendChild(detailFeelsLikeContainer);
+    detailFeelsLikeContainer.appendChild(detailFeelsLike);
+    detailFeelsLikeContainer.appendChild(detailFeelsLikeValue);
+    weatherDetailsContainer.appendChild(detailHumidityContainer);
+    detailHumidityContainer.appendChild(detailHumidity);
+    detailHumidityContainer.appendChild(detailHumidityValue);
+    weatherDetailsContainer.appendChild(detailDewPointContainer);
+    detailDewPointContainer.appendChild(detailDewPoint);
+    detailDewPointContainer.appendChild(detailDewPointValue);
+    weatherDetailsContainer.appendChild(detailPressureContainer);
+    detailPressureContainer.appendChild(detailPressure);
+    detailPressureContainer.appendChild(detailPressureValue);
+    weatherDetailsContainer.appendChild(detailUVIndexContainer);
+    detailUVIndexContainer.appendChild(detailUVIndex);
+    detailUVIndexContainer.appendChild(detailUVIndexValue);
+    weatherDetailsContainer.appendChild(detailWindSpeedContainer);
+    detailWindSpeedContainer.appendChild(detailWindSpeed);
+    detailWindSpeedContainer.appendChild(detailWindSpeedValue);
+    weatherDetailsContainer.appendChild(detailWindDirectionContainer);
+    detailWindDirectionContainer.appendChild(detailWindDirection);
+    detailWindDirectionContainer.appendChild(detailWindDirectionValue);
+    weatherDetailsContainer.appendChild(detailSunriseContainer);
+    detailSunriseContainer.appendChild(detailSunrise);
+    detailSunriseContainer.appendChild(detailSunriseValue);
+    weatherDetailsContainer.appendChild(detailSunsetContainer);
+    detailSunsetContainer.appendChild(detailSunset);
+    detailSunsetContainer.appendChild(detailSunsetValue);
 
     return weatherDetailsContainer;
   }
--- src/modules/openWeather.js
+++ src/modules/openWeather.js
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 /* eslint-disable consistent-return */
+import { fromUnixTime, format } from 'date-fns';
+
 class OpenWeather {
   static limit = 1;
 
@@ -15,6 +17,8 @@ class OpenWeather {
     this.units = units;
     this.unitSymbol = unitSymbol;
     this.temp = '';
+    this.hi = '';
+    this.low = '';
     this.feelsLike = '';
     this.humidity = '';
     this.dewpoint = '';
@@ -25,13 +29,14 @@ class OpenWeather {
     this.windDirection = '';
     this.sunrise = '';
     this.sunset = '';
-    this.day0 = '';
-    this.day1 = '';
-    this.day2 = '';
-    this.day3 = '';
-    this.day4 = '';
-    this.day5 = '';
-    this.day6 = '';
+    this.day0 = {};
+    this.day1 = {};
+    this.day2 = {};
+    this.day3 = {};
+    this.day4 = {};
+    this.day5 = {};
+    this.day6 = {};
+    this.day7 = {};
     this.description = '';
     this.icon = '';
   }
@@ -53,8 +58,10 @@ class OpenWeather {
         { mode: 'cors' },
       );
       const dataResponse = await openWeatherResponse.json();
-      // console.log(dataResponse);
+      console.log(dataResponse);
       this.temp = dataResponse.current.temp;
+      this.hi = dataResponse.daily[0].temp.max;
+      this.low = dataResponse.daily[0].temp.min;
       this.feelsLike = dataResponse.current.feels_like;
       this.humidity = dataResponse.current.humidity;
       this.dewpoint = dataResponse.current.dew_point;
@@ -67,8 +74,9 @@ class OpenWeather {
       this.description = dataResponse.current.weather[0].description;
       this.icon = dataResponse.current.weather[0].icon;
       // eslint-disable-next-line operator-linebreak
-      [, this.day0, this.day1, this.day2, this.day3, this.day4, this.day5, this.day6] =
+      [this.day0, this.day1, this.day2, this.day3, this.day4, this.day5, this.day6, this.day7] =
         dataResponse.daily;
+      this.cleanData();
       return dataResponse;
     } catch (error) {
       console.log(error);
@@ -78,12 +86,21 @@ class OpenWeather {
   cleanData() {
     const hPa_TO_inHg_Conversion_Number = 33.863886666667;
 
-    this.temp = `${this.temp.split('.')[0]}°${this.unitSymbol}`;
-    this.feelsLike = `${this.feelsLike.split('.')[0]}°${this.unitSymbol}`;
+    this.temp = `${`${this.temp}`.split('.')[0]}°${this.unitSymbol}`;
+    this.hi = `${`${this.hi}`.split('.')[0]}°${this.unitSymbol}`;
+    this.low = `${`${this.low}`.split('.')[0]}°${this.unitSymbol}`;
+    this.feelsLike = `${`${this.feelsLike}`.split('.')[0]}°${this.unitSymbol}`;
     this.humidity = `${this.humidity}%`;
-    this.dewpoint = `${this.dewpoint}°`;
-    this.pressure = `${this.pressure / hPa_TO_inHg_Conversion_Number} inHg`;
+    this.dewpoint = `${`${this.dewpoint}`.split('.')[0]}°${this.unitSymbol}`;
+    this.pressure = `${(this.pressure / hPa_TO_inHg_Conversion_Number).toPrecision(4)} inHg`;
     this.windSpeed = `${this.windSpeed} ${this.windSpeedUnits}`;
+
+    const days = [this.day1, this.day2, this.day3, this.day4, this.day5, this.day6, this.day7];
+    for (let i = 0; i < days.length; i += 1) {
+      days[i].temp.max = `${`${days[i].temp.max}`.split('.')[0]}°${this.unitSymbol}`;
+      days[i].temp.min = `${`${days[i].temp.min}`.split('.')[0]}°${this.unitSymbol}`;
+      days[i].dt = format(fromUnixTime(days[i].dt), 'E');
+    }
 
     switch (true) {
       case this.windDirection >= 348.75 && this.windDirection < 11.25:
@@ -135,6 +152,10 @@ class OpenWeather {
         this.windDirection = `${this.windDirection}`;
         break;
     }
+    // this.sunrise = new Date(this.sunrise).toLocaleString('en-US', {weekday: 'short'});
+
+    this.sunrise = format(fromUnixTime(this.sunrise), 'h:mm a');
+    this.sunset = format(fromUnixTime(this.sunset), 'h:mm a');
   }
 }
 export default OpenWeather;
